d4bc4645558336a651fc8ea21f1e5c1a535c90b7
Leonid <safronovleonid@gmail.com>
2021-10-21 08:43:51 +0300

    Initial Commit

---
+++ SMSTheBest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMSTheBest
+//
+//  Created by Леонид on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
