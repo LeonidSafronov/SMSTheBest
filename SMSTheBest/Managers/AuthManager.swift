37813a59e0aa3e708cc926bb1bf2d2da4a7f9342
LeonidSafronov <safronovleonid@gmail.com>
2021-11-16 17:16:07 +0300

    start auth

--- SMSTheBest/Managers/AuthManager.swift
+++ SMSTheBest/Managers/AuthManager.swift
@@ -10,10 +10,19 @@ import Foundation
 
 class AuthManager {
     
+    static let shared = AuthManager()
     private let auth = Auth.auth()
+    private var verificationId: String?
     
-    public func startAuth(phoneNumber: String?, completion: @escaping (Bool) -> Void) {
-        
+    public func startAuth(phoneNumber: String, completion: @escaping (Bool) -> Void) {
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { [weak self] verificationId, error in
+            guard let verificationId = verificationId, error == nil else {
+                completion(false)
+                return
+            }
+            self?.verificationId = verificationId
+            completion(true)
+        }
     }
     
     public func verifyCode(smsCode: String?, completion: @escaping (Bool) -> Void) {
